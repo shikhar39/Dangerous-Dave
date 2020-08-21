09c3976ba4a721cf26577b9c77f8efb53d934184
shikhar mishra <shikharmishra39@gmail.com>
2020-08-22 02:32:23 +0530

    Added texture and speed as members of Player class

--- Project1/src/Player.h
+++ Project1/src/Player.h
@@ -3,10 +3,14 @@
 
 class Player {
 public:
-	Player(sf::Vector2f size, sf::Vector2f position);
+	Player(sf::Texture* texture, sf::Vector2f size, sf::Vector2f position, float speed);
 	~Player();
+
+	void Update();
+
 private:
 	sf::RectangleShape body;
 	sf::Vector2f velocity;
 	bool facingRight;
+	float speed;
 };
